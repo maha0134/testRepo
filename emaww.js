6161ee171950cb2f1d8b01c7f82d42f2bd5f18f4
Akshay Mahajan <maha0134@algonquinlive.com>
2023-09-23 16:58:00 +0530

    Refactor code to separate into functions

--- emaww.js
+++ emaww.js
@@ -1,51 +1,45 @@
+//Imports
 const redis = require("redis");
+const fs = require("fs");
+//Node package at https://www.npmjs.com/package/xml2json
+const parser = require("xml2json");
+
+const filePath = "./config.xml";
 let finalKeys = {};
-function createRedisKeys() {
-  const fs = require("fs");
-  //Node package at https://www.npmjs.com/package/xml2json
-  const parser = require("xml2json");
-  const filePath = "./config.xml";
 
-  fs.readFile(filePath, "utf8", (err, data) => {
-    if (err) {
-      console.log("error reading the xml file: ", err);
-      return;
-    }
-    //As per documentation, setting object to true returns a JavaScript object
-    var options = {
-      object: true,
-      reversible: false,
-      coerce: false,
-      sanitize: true,
-      trim: true,
-      arrayNotation: false,
-      alternateTextNode: false,
-    };
+function parseXML(data) {
+  var options = {
+    object: true, //As per documentation, setting object to true returns a JavaScript object
+    reversible: false,
+    coerce: false,
+    sanitize: true,
+    trim: true,
+    arrayNotation: false,
+    alternateTextNode: false,
+  };
 
-    const jsObject = parser.toJson(data, options);
-    let subDomain = {};
-    const subDomains = jsObject.config.subdomains.subdomain;
-    subDomain.subdomains = subDomains;
-
-    const cookies = jsObject.config.cookies.cookie;
-    //Design the pattern for the keys of the cookies
-    const cookiesKey = cookies.map((cookie) => {
-      const redisKey = `cookie:${cookie.name}:${cookie.host}`;
-      const value = cookie["$t"];
-      return { [redisKey]: value };
-    });
-    //using reduce to collect each object and using the initial value of subDomain object to add onto it
-    finalKeys = cookiesKey.reduce((acc, cookie) => {
-      return { ...acc, ...cookie };
-    }, subDomain);
-    setUpRedis();
-  });
+  return parser.toJson(data, options);
 }
-async function setUpRedis() {
-  const client = redis.createClient({
-    url: "redis://redis:6379",
-  });
 
+function segregateKeys(jsObject) {
+  let subDomain = {};
+  const subDomains = jsObject.config.subdomains.subdomain;
+  subDomain.subdomains = subDomains;
+
+  const cookies = jsObject.config.cookies.cookie;
+  //Design the pattern for the keys of the cookies
+  const cookiesKey = cookies.map((cookie) => {
+    const redisKey = `cookie:${cookie.name}:${cookie.host}`;
+    const value = cookie["$t"];
+    return { [redisKey]: value };
+  });
+  //using reduce to collect each object and using the initial value of subDomain object to add onto it
+  return cookiesKey.reduce((acc, cookie) => {
+    return { ...acc, ...cookie };
+  }, subDomain);
+}
+
+async function setUpRedis(client, finalKeys) {
   client.on("error", (err) => console.log("Redis Client Error", err));
 
   await client.connect();
@@ -60,4 +54,23 @@ async function setUpRedis() {
   console.log(cookie); //mmm_for_dlp_777_ppc_m
 }
 
-createRedisKeys();
+function createRedisKeys(data, client) {
+  const jsObject = parseXML(data);
+  finalKeys = segregateKeys(jsObject);
+  setUpRedis(client, finalKeys);
+}
+
+//Usage
+
+fs.readFile(filePath, "utf8", (err, data) => {
+  if (err) {
+    console.log("error reading the xml file: ", err);
+    return;
+  }
+
+  const client = redis.createClient({
+    url: "redis://redis:6379",
+  });
+
+  createRedisKeys(data, client);
+});
